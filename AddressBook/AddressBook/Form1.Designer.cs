3c8aa44640b17875fc708fb345cd319442a9eb41
지현 <372491@naver.com>
2022-12-02 20:04:49 +0900

    C# 주소록 프로젝트

---
+++ AddressBook/AddressBook/Form1.Designer.cs
@@ -0,0 +1,247 @@
+﻿namespace AddressBook
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lvList = new System.Windows.Forms.ListView();
+            this.chId = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.chName = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.chAge = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.chTel = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.chJob = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnDel = new System.Windows.Forms.Button();
+            this.btnModify = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.txtAge = new System.Windows.Forms.TextBox();
+            this.txtTel = new System.Windows.Forms.TextBox();
+            this.txtJob = new System.Windows.Forms.TextBox();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lvList
+            // 
+            this.lvList.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.chId,
+            this.chName,
+            this.chAge,
+            this.chTel,
+            this.chJob});
+            this.lvList.HideSelection = false;
+            this.lvList.Location = new System.Drawing.Point(13, 13);
+            this.lvList.Name = "lvList";
+            this.lvList.Size = new System.Drawing.Size(716, 240);
+            this.lvList.TabIndex = 7;
+            this.lvList.UseCompatibleStateImageBehavior = false;
+            this.lvList.View = System.Windows.Forms.View.Details;
+            // 
+            // chId
+            // 
+            this.chId.Text = "구분";
+            this.chId.Width = 100;
+            // 
+            // chName
+            // 
+            this.chName.Text = "이름";
+            this.chName.Width = 180;
+            // 
+            // chAge
+            // 
+            this.chAge.Text = "나이";
+            this.chAge.Width = 160;
+            // 
+            // chTel
+            // 
+            this.chTel.Text = "전화번호";
+            this.chTel.Width = 250;
+            // 
+            // chJob
+            // 
+            this.chJob.Text = "직업";
+            this.chJob.Width = 220;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.Controls.Add(this.btnDel);
+            this.panel1.Controls.Add(this.btnModify);
+            this.panel1.Controls.Add(this.btnSave);
+            this.panel1.Controls.Add(this.txtAge);
+            this.panel1.Controls.Add(this.txtTel);
+            this.panel1.Controls.Add(this.txtJob);
+            this.panel1.Controls.Add(this.txtName);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(13, 259);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(716, 125);
+            this.panel1.TabIndex = 1;
+            // 
+            // btnDel
+            // 
+            this.btnDel.Location = new System.Drawing.Point(571, 71);
+            this.btnDel.Name = "btnDel";
+            this.btnDel.Size = new System.Drawing.Size(75, 23);
+            this.btnDel.TabIndex = 6;
+            this.btnDel.Text = "삭제";
+            this.btnDel.UseVisualStyleBackColor = true;
+            // 
+            // btnModify
+            // 
+            this.btnModify.Location = new System.Drawing.Point(571, 29);
+            this.btnModify.Name = "btnModify";
+            this.btnModify.Size = new System.Drawing.Size(75, 23);
+            this.btnModify.TabIndex = 5;
+            this.btnModify.Text = "수정";
+            this.btnModify.UseVisualStyleBackColor = true;
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(475, 23);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 74);
+            this.btnSave.TabIndex = 4;
+            this.btnSave.Text = "저장";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtAge
+            // 
+            this.txtAge.Location = new System.Drawing.Point(75, 72);
+            this.txtAge.Name = "txtAge";
+            this.txtAge.Size = new System.Drawing.Size(141, 25);
+            this.txtAge.TabIndex = 1;
+            // 
+            // txtTel
+            // 
+            this.txtTel.Location = new System.Drawing.Point(309, 29);
+            this.txtTel.Name = "txtTel";
+            this.txtTel.Size = new System.Drawing.Size(141, 25);
+            this.txtTel.TabIndex = 2;
+            // 
+            // txtJob
+            // 
+            this.txtJob.Location = new System.Drawing.Point(309, 72);
+            this.txtJob.Name = "txtJob";
+            this.txtJob.Size = new System.Drawing.Size(141, 25);
+            this.txtJob.TabIndex = 3;
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(75, 29);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(141, 25);
+            this.txtName.TabIndex = 0;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(236, 82);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(37, 15);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "직업";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(22, 82);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(37, 15);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "나이";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(236, 32);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(67, 15);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "전화번호";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(22, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(37, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "이름";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(745, 399);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.lvList);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "주소록";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView lvList;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnDel;
+        private System.Windows.Forms.Button btnModify;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.TextBox txtAge;
+        private System.Windows.Forms.TextBox txtTel;
+        private System.Windows.Forms.TextBox txtJob;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ColumnHeader chId;
+        private System.Windows.Forms.ColumnHeader chName;
+        private System.Windows.Forms.ColumnHeader chAge;
+        private System.Windows.Forms.ColumnHeader chTel;
+        private System.Windows.Forms.ColumnHeader chJob;
+    }
+}
+
---
+++ AddressBook/AddressBook/Form1.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace AddressBook
+{
+    public partial class Form1 : Form
+    {
+        string connStr = "Server=localhost;Database=address;Uid=root;Pwd=1234";
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(txtCheck() == true)
+            {
+                using (MySqlConnection conn = new MySqlConnection(connStr))
+                {
+                    conn.Open();
+
+                    String sql = "INSERT INTO t_info " +
+                                 "(name, phone, Job, age) " +
+                                 "VALUES('" + txtName.Text + "', '" + txtTel.Text + "', '"
+                                 + txtJob.Text + "', " + int.Parse(txtAge.Text) +")";
+                    Console.WriteLine(sql);
+
+                    MySqlCommand cmd = new MySqlCommand(sql, conn); 
+
+                    int i = cmd.ExecuteNonQuery();
+                    if(i == 1)
+                    {
+                        MessageBox.Show("정상적으로 테이터가 저장되었습니다.", "알림",
+                            MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                        lvList_DB_View();
+                        TxtClear();
+                        txtName.Focus();
+                    }
+                }
+            }
+        }
+
+        private void TxtClear()
+        {
+            txtName.Text = "";
+            txtTel.Text = "";
+            txtAge.Text = "";
+            txtJob.Text = "";
+        }
+
+        private bool txtCheck()
+        {
+            if(txtName.Text == "")
+            {
+                txtName.Focus();
+                return false;
+            }
+            if (txtAge.Text == "")
+            {
+                txtAge.Focus();
+                return false;
+            }
+            if (txtTel.Text == "")
+            {
+                txtTel.Focus();
+                return false;
+            }
+            if (txtJob.Text == "")
+            {
+                txtJob.Focus();
+                return false;
+            }
+            return true;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            lvList_DB_View();
+            txtName.Focus();
+        }
+
+        private void lvList_DB_View()
+        {
+            lvList.Items.Clear();
+
+            using(MySqlConnection conn = new MySqlConnection(connStr))
+            {
+                conn.Open();
+                String sql = "SELECT id, name, age, phone, job FROM t_info";
+                MySqlCommand cmd = new MySqlCommand(sql, conn);
+                MySqlDataReader dr = cmd.ExecuteReader();
+
+                while (dr.Read())
+                {
+                    lvList.Items.Add(new ListViewItem(new String[]
+                    {
+                        dr[0].ToString(),
+                        dr[1].ToString(),
+                        dr[2].ToString(),
+                        dr[3].ToString(),
+                        dr[4].ToString()
+                        }));
+                }
+                dr.Close();
+                conn.Close();
+            }
+        }
+    }
+}
